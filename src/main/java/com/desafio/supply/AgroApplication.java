47b92aabcfd601ffc7a07e1b1d77b7a0c6af6390
Alisson Zubcov <alissonrichardy@hotmail.com>
2020-11-30 21:52:02 -0300

    primeiro commit

---
+++ src/main/java/com/desafio/supply/AgroApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.supply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroApplication.class, args);
+	}
+
+}
